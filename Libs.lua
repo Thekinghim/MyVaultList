27957901aeb562171eed29c72d2b7ab8d7005c84
Wilsterb <151988881+Thekinghim@users.noreply.github.com>
2024-03-27 14:11:10 +0100

    Add files via upload

---
+++ Libs.lua
@@ -0,0 +1,8 @@
+---@class Arenalogs
+local Arenalogs = select(2, ...)
+
+---@type GGUI-2.1
+Arenalogs.GGUI = LibStub:GetLibrary("GGUI-2.1")
+
+---@type GUTIL-2.0
+Arenalogs.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
