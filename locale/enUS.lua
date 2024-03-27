f4e8f35c365ef4b1b2a02c04e2f075b4b3f5fa00
Wilsterb <151988881+Thekinghim@users.noreply.github.com>
2024-03-27 14:09:27 +0100

    Add files via upload

---
+++ locale/enUS.lua
@@ -0,0 +1,14 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MyVaultList", "enUS", true)
+
+if not L then
+    return
+end
+
+L["Character"] = "Character"
+L["Raids"] = "Raids"
+L["Activities"] = "Activities"
+L["PvP"] = "PvP"
+L["iLevel"] = "Item Level"
+L["Mythic"] = "Mythic"
+L["Boss List"] = "Boss List"
+L["Keys"] = "Keys"
